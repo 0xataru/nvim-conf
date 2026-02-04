89903d915aa3cd0c3b8304527fea1da8f3a2a0e0
Mark Raiter <ataru@Marks-MacBook-Air.local>
2026-02-04 13:42:35 +0200

    fix colors for yank highlight

--- lua/ataru/core/autocmd.lua
+++ lua/ataru/core/autocmd.lua
@@ -1,11 +1,25 @@
 local ag = vim.api.nvim_create_augroup
 
-local highlight_group = ag("YankHighlight", { clear = true })
+local function set_yank_highlight()
+    vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#e67e22" })
+end
+
+-- Apply once on load (in case autocmd runs after colorscheme)
+set_yank_highlight()
+
+-- Re-apply after colorscheme loads so it is not overwritten
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = set_yank_highlight,
+    group = ag("YankHighlightRefresh", { clear = true }),
+})
+
+local yank_augroup = ag("YankHighlight", { clear = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
     pattern = "*",
     callback = function()
-        vim.highlight.on_yank({ timeout = 170 })
+        vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 170 })
     end,
-    group = highlight_group,
+    group = yank_augroup,
 })
