ef11abb2c4c1353e4b62858ffb7c1c30f5df3856
Mark Raiter <ataru@Marks-MacBook-Air.local>
2026-02-04 12:03:42 +0200

    fix lualine

--- lua/ataru/plugins/lualine.lua
+++ lua/ataru/plugins/lualine.lua
@@ -2,6 +2,15 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+        -- Get foreground color of a highlight group (no external deps like Snacks)
+        local function hl_fg(name)
+            local hl = vim.api.nvim_get_hl(0, { name = name })
+            if hl and hl.fg then
+                return string.format("#%06x", hl.fg)
+            end
+            return nil
+        end
+
         require("lualine").setup({
             options = {
                 theme = "vscode",
@@ -40,11 +49,45 @@ return {
                 },
                 lualine_x = {
                     "diagnostics",
+                    "diff",
+                    {
+                        function()
+                            return require("noice").api.status.command.get()
+                        end,
+                        cond = function()
+                            return package.loaded["noice"] and require("noice").api.status.command.has()
+                        end,
+                        color = function()
+                            local fg = hl_fg("Statement")
+                            return fg and { fg = fg } or {}
+                        end,
+                    },
+                    {
+                        function()
+                            return require("noice").api.status.mode.get()
+                        end,
+                        cond = function()
+                            return package.loaded["noice"] and require("noice").api.status.mode.has()
+                        end,
+                        color = function()
+                            local fg = hl_fg("Constant")
+                            return fg and { fg = fg } or {}
+                        end,
+                    },
+                    {
+                        require("lazy.status").updates,
+                        cond = require("lazy.status").has_updates,
+                        color = function()
+                            local fg = hl_fg("Special")
+                            return fg and { fg = fg } or {}
+                        end,
+                    },
                 },
                 lualine_y = {
                     "filetype",
                 },
                 lualine_z = {
+                    -- { "progress", separator = " ", padding = { left = 1, right = 0 } },
                     "location",
                 },
             },
