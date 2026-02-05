3084af77463d8a67c853d255d68f9cc7e2162f06
Mark Raiter <ataru@Marks-MacBook-Air.local>
2026-02-05 14:20:07 +0200

    fix colorscheme

--- lua/ataru/plugins/colorscheme.lua
+++ lua/ataru/plugins/colorscheme.lua
@@ -4,36 +4,36 @@ return {
     config = function()
         vim.cmd("colorscheme vscode")
 
-        -- transparent background
-        vim.cmd([[
-        highlight Normal guibg=NONE ctermbg=NONE
-        highlight NonText guibg=NONE ctermbg=NONE
-
-        highlight NeoTreeNormal guibg=NONE ctermbg=NONE
-        highlight NeoTreeFloatNormal guibg=NONE ctermbg=NONE
-        highlight NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
-        highlight NeoTreeCursorLine guibg=NONE ctermbg=NONE
-
-        highlight VertSplit guibg=NONE ctermbg=NONE
-        highlight SignColumn guibg=NONE ctermbg=NONE
-        highlight EndOfBuffer guibg=NONE ctermbg=NONE
-        highlight LineNr guibg=NONE ctermbg=NONE
-        highlight CursorLineNr guibg=NONE ctermbg=NONE
-        highlight Pmenu guibg=NONE ctermbg=NONE
-        highlight PmenuSel guibg=NONE ctermbg=NONE
-        highlight FloatBorder guibg=NONE ctermbg=NONE
-        highlight NormalFloat guibg=NONE ctermbg=NONE
-        highlight StatusLine guibg=NONE ctermbg=NONE
-        highlight StatusLineNC guibg=NONE ctermbg=NONE
-        highlight TabLine guibg=NONE ctermbg=NONE
-        highlight TabLineFill guibg=NONE ctermbg=NONE
-        highlight TabLineSel guibg=NONE ctermbg=NONE
-        highlight WinSeparator guibg=NONE ctermbg=NONE
-      ]])
-
         -- Cursor line and 140-column ruler (same color, applied after theme)
         local line_highlight = "#4a4a4a"
         vim.api.nvim_set_hl(0, "CursorLine", { bg = line_highlight })
         vim.api.nvim_set_hl(0, "ColorColumn", { bg = line_highlight })
+
+        -- transparent background
+        -- vim.cmd([[
+        --   highlight Normal guibg=NONE ctermbg=NONE
+        --   highlight NonText guibg=NONE ctermbg=NONE
+        --
+        --   highlight NeoTreeNormal guibg=NONE ctermbg=NONE
+        --   highlight NeoTreeFloatNormal guibg=NONE ctermbg=NONE
+        --   highlight NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+        --   highlight NeoTreeCursorLine guibg=NONE ctermbg=NONE
+        --
+        --   highlight VertSplit guibg=NONE ctermbg=NONE
+        --   highlight SignColumn guibg=NONE ctermbg=NONE
+        --   highlight EndOfBuffer guibg=NONE ctermbg=NONE
+        --   highlight LineNr guibg=NONE ctermbg=NONE
+        --   highlight CursorLineNr guibg=NONE ctermbg=NONE
+        --   highlight Pmenu guibg=NONE ctermbg=NONE
+        --   highlight PmenuSel guibg=NONE ctermbg=NONE
+        --   highlight FloatBorder guibg=NONE ctermbg=NONE
+        --   highlight NormalFloat guibg=NONE ctermbg=NONE
+        --   highlight StatusLine guibg=NONE ctermbg=NONE
+        --   highlight StatusLineNC guibg=NONE ctermbg=NONE
+        --   highlight TabLine guibg=NONE ctermbg=NONE
+        --   highlight TabLineFill guibg=NONE ctermbg=NONE
+        --   highlight TabLineSel guibg=NONE ctermbg=NONE
+        --   highlight WinSeparator guibg=NONE ctermbg=NONE
+        -- ]])
     end,
 }
