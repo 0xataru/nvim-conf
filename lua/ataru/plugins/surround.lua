1270ac5c5af5c8b0d4700db2fb7b0278dad9c9f1
Mark Raiter <ataru@Marks-MacBook-Air.local>
2026-05-27 11:24:52 +0200

    add comment to surround with keymaps

--- lua/ataru/plugins/surround.lua
+++ lua/ataru/plugins/surround.lua
@@ -1,3 +1,19 @@
+-- nvim-surround keymaps (all default, no custom config)
+--
+-- NORMAL MODE:
+--   ys{motion}{char}  - add surround        e.g. ysiw"  -> "word",  ysip<div> -> <div>paragraph</div>
+--   yss{char}         - surround whole line  e.g. yss)   -> (line)
+--   ds{char}          - delete surround      e.g. ds"    -> removes surrounding quotes
+--   cs{old}{new}      - change surround      e.g. cs"'   -> "text" -> 'text',  cs"<em> -> <em>text</em>
+--
+-- VISUAL MODE:
+--   S{char}           - surround selection
+--   gS{char}          - surround selection with newlines
+--
+-- INSERT MODE:
+--   <C-g>s{char}      - insert surround pair and place cursor inside
+--   <C-g>S{char}      - same but with newlines
+
 return {
     "kylechui/nvim-surround",
     event = { "BufreadPre", "BufNewFile" },
